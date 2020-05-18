5bc1497ee3825954fcbe4531d0de8107d7d425af
Hassan Habib <hassanhabib@live.com>
2020-05-17 18:02:21 -0700

    Add project files.

---
+++ GraphODataDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Graph;
+using Microsoft.Graph.Auth;
+using Microsoft.Identity.Client;
+
+namespace GraphODataDemo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var confidentialClientApplication = ConfidentialClientApplicationBuilder
+               .Create("YOUR_CLIENT_ID")
+               .WithTenantId("YOUR_TENANT_ID")
+               .WithClientSecret("YOUR_APP_SECRET")
+               .Build();
+
+            var clientCredentialProvider = 
+                new ClientCredentialProvider(confidentialClientApplication);
+            
+            var graphServiceClient = 
+                new GraphServiceClient(clientCredentialProvider);
+
+            var users = await graphServiceClient.Users.Request().Filter("displayName eq 'Hassan Habib'").GetAsync();
+
+            foreach(var user in users)
+            {
+                Console.WriteLine(user.DisplayName);
+            }
+        }
+    }
+}
